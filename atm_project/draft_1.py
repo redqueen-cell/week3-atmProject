6e761b839a9875cde0f4896cb27fb2fee728b426
Sophia Velásquez <52333157+sophia-velasquez@users.noreply.github.com>
2021-04-22 17:56:48 -0700

    Delete draft_1.py

--- atm_project/draft_1.py
+++
@@ -1,7 +0,0 @@
-# create an auth session file when user is logged in
-# delete the file when user logs out
-
-auth_session_path = "data/auth_session/"
-
-f1 = open(auth_session_path + "loginsession.txt", "w")
-f1.write("User Blank is logged in")
